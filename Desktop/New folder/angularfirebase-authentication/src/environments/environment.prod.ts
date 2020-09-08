e8a90835e0651460f28275c219f6be2595b57d5e
pratikgosavii <pratikgosavi654@gmail.com>
2020-09-08 20:31:20 +0530

    sdsd

---
+++ Desktop/New folder/angularfirebase-authentication/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCywNPkHwl4_6YSFhKhheVVUHqPqKQE4mI",
+    authDomain: "demobohocompany-1e963.firebaseapp.com",
+    databaseURL: "https://demobohocompany-1e963.firebaseio.com",
+    projectId: "demobohocompany-1e963",
+    storageBucket: "demobohocompany-1e963.appspot.com",
+    messagingSenderId: "352737904067",
+    appId: "1:352737904067:web:9dd4b317d44fe830cbe976",
+    measurementId: "G-X0H3PYPFYH"
+  }
+};
---
+++ Desktop/New folder/angularfirebase-authentication/src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCywNPkHwl4_6YSFhKhheVVUHqPqKQE4mI",
+    authDomain: "demobohocompany-1e963.firebaseapp.com",
+    databaseURL: "https://demobohocompany-1e963.firebaseio.com",
+    projectId: "demobohocompany-1e963",
+    storageBucket: "demobohocompany-1e963.appspot.com",
+    messagingSenderId: "352737904067",
+    appId: "1:352737904067:web:9dd4b317d44fe830cbe976",
+    measurementId: "G-X0H3PYPFYH"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
